6ee107660145db82777cb5fa586c033ef5dcf7ef
gunthergl_r1 <gunthergl@gmx.net>
2024-05-08 11:45:54 +0200

    starting the shiny app

--- R/app_ui.R
+++ R/app_ui.R
@@ -5,14 +5,41 @@
 #' @import shiny
 #' @noRd
 app_ui <- function(request) {
-  tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # Your application UI logic
-    fluidPage(
-      h1("golem.rROC")
+    tagList(
+        # Leave this function for adding external resources
+        golem_add_external_resources(),
+        # Your application UI logic
+        tabPanel(
+            "DT",
+            h2("A Random DT"),
+            DT::DTOutput("data_table")
+        ),
+        tabPanel(
+            "Image",
+            h2("A Random Image"),
+            plotOutput("image")
+        ),
+        tabPanel(
+            "Plot",
+            h2("A Random Plot"),
+            plotOutput("plot")
+        ),
+        tabPanel(
+            "Print",
+            h2("A Random Print"),
+            verbatimTextOutput("print")
+        ),
+        tabPanel(
+            "Table",
+            h2("A Random Table"),
+            tableOutput("table")
+        ),
+        tabPanel(
+            "Text",
+            h2("A Random Text"),
+            tableOutput("text")
+        )
     )
-  )
 }
 
 #' Add external Resources to the Application
@@ -24,18 +51,18 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
-
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "golem.rROC"
+    add_resource_path(
+        "www",
+        app_sys("app/www")
     )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert()
-  )
+
+    tags$head(
+        favicon(),
+        bundle_resources(
+            path = app_sys("app/www"),
+            app_title = "golem.rROC"
+        )
+        # Add here other external resources
+        # for example, you can add shinyalert::useShinyalert()
+    )
 }
