065b49fe076a70ca8a7a5e1ddeb96f937d8ae0d0
gunthergl_r1 <gunthergl@gmx.net>
2024-12-30 15:10:17 +0100

    continuous running

--- dev/continuous_run.sh
+++ dev/continuous_run.sh
@@ -1,39 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e # exit on errors
-# kill all processes where grep finds "run_dev.R"
-app_name="app.R"
-killing_processes=$(ps aux | grep $app_name)
-echo "Killing processes: $killing_processes"
-kill $(echo $killing_processes | awk '{print $2}') || echo "No processes to kill"
+# Path to the R folder
+WATCH_DIR="R/"
 
+# Command to run the R script
 
-# Batch changes every 1second
-next_allowed_run=$(date +%s)
-batch_window_seconds=1
-current_shiny_pid="EMPTY"
-inotifywait \
-    --monitor ./R \
-    --recursive \
-    --event=create \
-    --event=modify \
-    --event=attrib \
-    --event=delete \
-    --event=move \
-    --format='%T' \
-    --timefmt='%s' |
-    while read event_time; do
-        echo ${file_path}${file_name} event: ${file_event}
-        # If events arrive before the next allowed command-run, just skip them.
-        if [[ $event_time -ge $next_allowed_run ]]; then
-            echo "    DO THE EVENT!"
-            if [[ $current_shiny_pid != "EMPTY" ]]; then
-                echo "    KILLING SHINY SERVER: $current_shiny_pid"
-                kill $current_shiny_pid || echo "Process was not running"
-            fi
-            Rscript -e "source('./dev/$app_name')" &
-            current_shiny_pid=$!
-            next_allowed_run=$(date --date="${batch_window_seconds}sec" +%s)
-            sleep $batch_window_seconds # Wait for additional changes
-        fi
-    done
+R_SCRIPT="./dev/app.R"
+
+# Function to start the R script
+restart_r_script() {
+    # ps aux | grep "$R_SCRIPT"
+    # ps aux | grep "$R_SCRIPT" | awk '{print $2}' 
+    # ps aux | grep "$R_SCRIPT" | awk '{print $2}' | xargs -r kill # Kill any existing instances
+    ps aux | grep "$R_SCRIPT"
+    pkill -f "$R_SCRIPT" 2>/dev/null  # Kill any existing instances
+    Rscript $R_SCRIPT &  # Start the script in the background
+    # echo "R script started with PID $!"
+}
+
+# Initial start of the R script
+restart_r_script
+
+# Monitor the folder for changes
+inotifywait -m -e close_write "$WATCH_DIR" | while read -r path event file; do
+    echo "Detected change in $file. Restarting R script..."
+    restart_r_script
+done
