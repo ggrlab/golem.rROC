00a4ad9c3ca1c7796139e8b3f7372025c91107e9
gunthergl_r1 <gunthergl@gmx.net>
2024-12-30 13:39:07 +0100

    docker creation seems done?

--- create_docker.sh
+++ create_docker.sh
@@ -1,8 +1,8 @@
-docker build --file Dockerfile -t registry.gunthergl.com/golem.rroc:0.1 .
+docker build --file Dockerfile -t registry.gunthergl.com/golem.rroc:0.2 .
 docker build --file Dockerfile -t registry.gunthergl.com/golem.rroc:latest .
 
-docker push registry.gunthergl.com/golem.rroc:0.1
+docker push registry.gunthergl.com/golem.rroc:0.2
 docker push registry.gunthergl.com/golem.rroc:latest
 # docker push registry.gunthergl.com/golem.rroc:latest
 # docker run --rm -it --entrypoint /bin/bash registry.gunthergl.com/golem.rroc:0.1
-docker run -p 9207:9207 -it --rm registry.gunthergl.com/golem.rroc:0.1
+docker run -p 9207:9207 -it --rm registry.gunthergl.com/golem.rroc:0.2
