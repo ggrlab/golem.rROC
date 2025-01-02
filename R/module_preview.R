cbf3108a835955d3b74e806efa0b37e61b477ff6
gunthergl_r1 <gunthergl@gmx.net>
2025-01-02 15:32:41 +0100

    preview Module

---
+++ R/module_preview.R
@@ -0,0 +1,35 @@
+#' @title Preview data
+#' @description This module provides a preview of the first 6 columns of a dataset.
+#' @param id A character string representing the module's ID.
+#' @return A Shiny module for previewing data.
+previewDataUI <- function(id) {
+    tabsetPanel(
+        id = "tabs_data",
+        tabPanel(
+            "View first 6 columns",
+            DT::DTOutput(NS(id, "data_preview"))
+        ),
+        tabPanel(
+            "View full",
+            DT::DTOutput(NS(id, "data_preview_full"))
+        )
+    )
+}
+#' @noRd
+#' @export
+#' @examples
+#' previewDataUI("preview")
+previewDataServer <- function(id, data) {
+    stopifnot(is.reactive(data))
+    moduleServer(id, function(input, output, session) {
+        output$data_preview <- DT::renderDT({
+            data()[, seq_len(min(ncol(data()), 6))]
+        })
+        output$data_preview_full <- DT::renderDT(
+            {
+                data()
+            },
+            filter = "top"
+        )
+    })
+}
