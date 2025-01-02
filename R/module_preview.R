08ef4a7f778068e7bc5a609568cbbe3fe931bfe4
gunthergl_r1 <gunthergl@gmx.net>
2025-01-02 15:50:59 +0100

    update preview module

--- R/module_preview.R
+++ R/module_preview.R
@@ -21,26 +21,26 @@ previewDataUI <- function(id) {
 #' previewDataUI("preview")
 previewDataServer <- function(id, data) {
     stopifnot(is.reactive(data))
-    moduleServer(id, function(input, output, session) {
-        current_data_table <- reactive({
-            if (is.null(data())) {
-                return(NULL)
-            }
-            if (is.data.frame(data())) {
-                return(data())
-            } else if (is.list(data())) {
-                df_long <- data.table::rbindlist(data(), idcol = "group")
-                colnames(df_long) <- c("group", "value")
-                return(df_long)
-            }
-        })
+    data_table <- reactive({
+        if (is.null(data())) {
+            return(NULL)
+        }
+        if (is.data.frame(data())) {
+            return(data())
+        } else if (is.list(data())) {
+            df_long <- data.table::rbindlist(data(), idcol = "group")
+            colnames(df_long) <- c("group", "value")
+            return(df_long)
+        }
+    })
 
+    moduleServer(id, function(input, output, session) {
         output$data_preview <- DT::renderDT({
-            data()[, seq_len(min(ncol(data()), 6))]
+            data_table()[, seq_len(min(ncol(data_table()), 6))]
         })
         output$data_preview_full <- DT::renderDT(
             {
-                data()
+                data_table()
             },
             filter = "top"
         )
