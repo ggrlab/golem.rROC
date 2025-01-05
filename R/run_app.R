ce9250da001e01fcb275c41d2fa5065dbd7309af
gunthergl_r1 <gunthergl@gmx.net>
2025-01-05 17:14:55 +0100

    run app with n_daemons

--- R/run_app.R
+++ R/run_app.R
@@ -11,16 +11,19 @@ run_app <- function(onStart = NULL,
                     options = list(),
                     enableBookmarking = NULL,
                     uiPattern = "/",
+                    n_daemons = 3,
                     ...) {
-    with_golem_options(
-        app = shinyApp(
-            ui = app_ui,
-            server = app_server,
-            onStart = onStart,
-            options = options,
-            enableBookmarking = enableBookmarking,
-            uiPattern = uiPattern
-        ),
-        golem_opts = list(...)
-    )
+    with(mirai::daemons(n_daemons), {
+        with_golem_options(
+            app = shinyApp(
+                ui = app_ui,
+                server = app_server,
+                onStart = onStart,
+                options = options,
+                enableBookmarking = enableBookmarking,
+                uiPattern = uiPattern
+            ),
+            golem_opts = list(...)
+        )
+    })
 }
