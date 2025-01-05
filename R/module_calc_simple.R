f122869e43b909812bb4110be1035a5a385df969
gunthergl_r1 <gunthergl@gmx.net>
2025-01-05 16:27:01 +0100

    module_calc simple

--- R/module_calc_simple.R
+++ R/module_calc_simple.R
@@ -19,24 +19,23 @@ simpleET_server <- function(id, a, b) {
     stopifnot(is.reactive(a))
     stopifnot(is.reactive(b))
 
-    rand_task <- ExtendedTask$new(function(f_a, f_b, ...) {
-        # Get all arguments and ensure that they are NOT reactive. Necessary for ExtendedTask
-        argg <- c(as.list(environment()), list(...))
-        lapply(argg, function(x) {
-            stopifnot(!is.reactive(x))
-        })
-
-        future::future(
+    rand_task <- ExtendedTask$new(function(...) {
+        mirai::mirai(
             {
+                # Get all arguments and ensure that they are NOT reactive. Necessary for ExtendedTask
+                argg <- c(as.list(environment()))
+                lapply(argg, function(x) {
+                    stopifnot(!is.reactive(x))
+                })
+
                 print(f_a, f_b)
                 # Slow operation goes here
                 Sys.sleep(2)
                 sample(1:100, 1)
             },
-            seed = TRUE
+            ...
         )
-    })
-    bslib::bind_task_button(rand_task, NS(id, "recalculate"))
+    }) |> bslib::bind_task_button(NS(id, "recalculate"))
     moduleServer(id, function(input, output, session) {
         observe_simple(input, output, rand_task, a, b)
         output$out_a <- shiny::renderText({
@@ -48,7 +47,6 @@ simpleET_server <- function(id, a, b) {
         output$output_et <- renderText({
             paste0("Output is '", rand_task$result(), "'")
         })
-        
     })
 }
 
@@ -63,7 +61,7 @@ observe_simple <- function(input, output, exttask, a, b) {
         #     detail = "",
         #     value = 0,
         #     {
-        exttask$invoke(a(), b())
+        exttask$invoke(f_a = a(), f_b = b())
         #     })
     })
 }
