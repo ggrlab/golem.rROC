e56557208fea9bdc7250cbf33d1232048b72ec45
gunthergl_r1 <gunthergl@gmx.net>
2025-01-05 17:13:55 +0100

    remove simple extendedTask module in UI

--- R/module_calc_simple.R
+++ R/module_calc_simple.R
@@ -19,49 +19,82 @@ simpleET_server <- function(id, a, b) {
     stopifnot(is.reactive(a))
     stopifnot(is.reactive(b))
 
-    rand_task <- ExtendedTask$new(function(...) {
-        mirai::mirai(
-            {
-                # Get all arguments and ensure that they are NOT reactive. Necessary for ExtendedTask
-                argg <- c(as.list(environment()))
-                lapply(argg, function(x) {
-                    stopifnot(!is.reactive(x))
-                })
+    # rand_task <- ExtendedTask$new(function(...) {
+    #     mirai::mirai(
+    #         {
+    #             # Get all arguments and ensure that they are NOT reactive. Necessary for ExtendedTask
+    #             argg <- c(as.list(environment()))
+    #             lapply(argg, function(x) {
+    #                 stopifnot(!is.reactive(x))
+    #             })
 
-                print(f_a, f_b)
-                # Slow operation goes here
-                Sys.sleep(2)
-                sample(1:100, 1)
-            },
-            ...
+    #             print(f_a, f_b)
+    #             # Slow operation goes here
+    #             Sys.sleep(2)
+    #             sample(1:100, 1)
+    #         },
+    #         ...
+    #     )
+    # }) |> bslib::bind_task_button(NS(id, "recalculate"))
+    myfunc <- function(x, y) {
+        # Get all arguments and ensure that they are NOT reactive. Necessary for ExtendedTask
+        argg <- c(as.list(environment()))
+        lapply(argg, function(x) {
+            stopifnot(!is.reactive(x))
+        })
+
+        # print(x, y)
+        # Slow operation goes here
+        Sys.sleep(2)
+        paste0("___", x, y, sample(1:100, 1))
+    }
+    rand_task <- ExtendedTask$new(function(fun, x, y) {
+        mirai::mirai(
+            fun(
+                x = x, y = y
+            ),
+            environment()
         )
     }) |> bslib::bind_task_button(NS(id, "recalculate"))
     moduleServer(id, function(input, output, session) {
-        observe_simple(input, output, rand_task, a, b)
+        observe_simple(input, output, rand_task, a, b, myfunc)
         output$out_a <- shiny::renderText({
             paste0("A is '", a(), "'")
         })
         output$out_b <- renderText({
             paste0("B is '", b(), "'")
         })
-        output$output_et <- renderText({
-            paste0("Output is '", rand_task$result(), "'")
+
+        observeEvent(rand_task$status(), {
+            print(Sys.time())
+            cat(": ", "RROC calculation status: ")
+            print(str(rand_task$status()))
+            if (rand_task$status() == "success") {
+                output$output_et <- renderText({
+                    paste0("Output is '", rand_task$result(), "'")
+                })
+            } else if (rand_task$status() == "failed") {
+                output$output_et <- renderText({
+                    paste0("Output is '", rand_task$result(), "'")
+                })
+            }
+            rand_task$result()
         })
     })
 }
 
 
-observe_simple <- function(input, output, exttask, a, b) {
+observe_simple <- function(input, output, exttask, a, b, myfunc) {
     stopifnot(is.reactive(a))
     stopifnot(is.reactive(b))
-
+    b(0)
     observeEvent(input$recalculate, {
         # shiny::withProgress(
         #     message = "Calculating restriction...",
         #     detail = "",
         #     value = 0,
         #     {
-        exttask$invoke(f_a = a(), f_b = b())
+        exttask$invoke(myfunc, x = paste0("A=", a()), y = paste0("B=", b()))
         #     })
     })
 }
